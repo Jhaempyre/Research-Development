79f779ea63a2871578c239e2b130427ed4d43100
jhaemyre <jhaashish.ajha@gmail.com>
2024-04-10 02:21:03 +0530

    first commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,131 @@
+import bcrypt from "bcrypt";
+import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/genrateToken.js";
+
+const registerUser = async(req, res) => {
+   try {
+     const {fullName, username, password, email,confirmPassword, gender} = req.body
+    console.log(req.body);
+     if (password !== confirmPassword) {
+         return res.status(400).json({ error: "Passwords don't match" });
+     }
+ 
+     const user = await User.findOne({ username });
+     console.log(1)
+ 
+     if (user) {
+         return res.status(400).json({ error: "Username already exists" });
+     }
+     console.log(2)
+     const salt = await bcrypt.genSalt(10);
+     console.log(9)
+     const hashedPassword = await bcrypt.hash(password, salt);
+     console.log(10)
+     console.log(hashedPassword)
+     console.log(3)
+ 
+     const boyAvtar= `https://avatar.iran.liara.run/public/boy?username=${username}`
+     const girlAvtar = `https://avatar.iran.liara.run/public/girl?username=${username}`
+     console.log(4)
+     const newUser = new User({
+         username, 
+         fullName,
+         gender ,
+         email ,
+         password : hashedPassword,
+         profilePic: gender === 'male'? boyAvtar : girlAvtar
+     })
+     console.log(5)
+
+      console.log(newUser);
+
+     if (newUser){
+         generateTokenAndSetCookie(newUser._id, res);
+         await newUser.save();
+ 
+         res.status(200).json({
+             id : newUser._id,
+             Name : newUser.fullName,
+             Gender : newUser.gender,
+             Email : newUser.email,
+             profilePic : newUser.profilePic,
+             username: newUser.username,
+         })
+ 
+     }else {
+         res.status(400).json({ error: "Invalid user data" })
+   } 
+}
+   catch (error) {
+    console.log("Error in signup controller", error.message);
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+}
+    
+};
+
+
+
+const loginUser = async(req,res)=>{
+    try {
+        const  { password , username } = req.body;
+        console.log( req.body);
+        // check if the user exists or not
+        let user = await User.findOne({ username });
+        console.log(user);
+        if (!user) return res.status(401).json({ error:"Username does not exist!"});
+      
+        // compare the given password with the saved one
+        const isValidPass=await bcrypt.compare(password, user.password);
+        if(!isValidPass ) return res.status(401).json({ error: "Wrong Password!" });
+        delete user.password
+        generateTokenAndSetCookie(user._id,res)
+        res.status(200).json({
+            message :"user logged in ",
+            info :{
+                _id: user._id,
+                fullName: user.fullName,
+                username: user.username,
+                Avatar : user.avatar,
+            }
+        })     
+    } catch (error) {
+        console.log("Error in login, please try again ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+const logOut = async(req,res)=>{
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json(
+            { message: "Logged out successfully" }
+            );
+    } catch (error) {
+        console.log("Error in login, please try again ", error.message);
+        
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+const listOfSideBar = async(req,res)=>{
+try {
+    
+        const foundList = req.user_id
+        const notFound = await User.find({_id:{$ne :foundList}})
+                                .select('-password');
+        res.status(200).json(notFound);
+} catch (error) {
+    console.log("Bhavyaaa is paidal",error.message)
+}
+
+    
+}
+export{
+    registerUser,
+    loginUser,
+    logOut ,
+    listOfSideBar
+}
+
+  
---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose , {Schema} from "mongoose";
+
+
+const userSchema = new Schema(
+    {
+        email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase :true,
+        trim:true,
+        },
+        password:{
+        type: String,  
+        required:[true,"Passowrd caahiye re baba"]
+        },
+        confirmPassword :{
+            type : String ,
+            required : [ false ,"Confirm Password"]
+        },
+        username: {
+			type: String,
+			required: true,
+			unique: true,
+        },
+        fullName :{
+            type : String ,
+            required : [ true ,"Full name is required" ] 
+        },
+        gender: {
+			type: String,
+			required: true,
+			enum: ["male", "female"],
+		},
+        profilePic: {
+			type: String,
+			default: "",
+		},
+        mobile : {
+            type : Number ,
+            required : [true , "Phone number is must"]
+        }
+
+},{
+    timestamps:true
+})
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
