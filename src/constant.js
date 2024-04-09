072cedba21598776b3baceec9a2348c8ea616540
jhaemyre <jhaashish.ajha@gmail.com>
2024-04-10 04:03:50 +0530

    Not intended but connected databse using dynamnic names

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,21 @@
-export const DB_NAME = 'research-development'
+// Define the function to extract mobile number from an object
+function getMobileNumber(req) {
+    // Destructure the 'mobile' property from the object
+    const { mobile } = req;
+
+    // Return the value of 'mobile'
+    return mobile;
+}
+
+// Define the data object
+const data = {
+    mobile: '123-456-7890',
+    // other properties...
+};
+
+// Call the function to extract the mobile number
+const mobileNumber = getMobileNumber(data);
+console.log(mobileNumber); // Output: 123-456-7890
+
+// Export the function if you're using ES6 module syntax
+export default mobileNumber;
--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,10 +1,13 @@
 import mongoose, { connect } from "mongoose";
-import { DB_NAME } from "../constant.js";
+import DB_NAME from "../constant.js";
+
 
 
 const connectDB = async()=>{
     try {
+        console.log('DB_NAME:', DB_NAME); // Log the value of DB_NAME
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
         
     } catch (error) {
